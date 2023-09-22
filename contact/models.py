562fc537f27fda10b1da9752eb84bf1b5aeab6d2
Jimmy <an.9647873111@gmail.com>
2023-09-22 15:40:55 +0200

    cook-blog

--- contact/models.py
+++ contact/models.py
@@ -5,7 +5,7 @@ class ContactModel(models.Model):
     """Класс модели обратной связи"""
     name = models.CharField(max_length=50)
     email = models.EmailField()
-    website = models.URLField()
+    website = models.URLField(blank=True, null=True)
     message = models.TextField(max_length=5000)
     create_at = models.DateTimeField(auto_now_add=True)
 
